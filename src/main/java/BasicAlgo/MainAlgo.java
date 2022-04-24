9499d3c7c5dc6519001f67e6abe7225a5e1b2e9c
ilyas <larhdidok@gmail.com>
2022-04-24 02:39:15 +0100

    all ones problem using GeneticAlgorithm (in the CrossOver stage now)

---
+++ src/main/java/BasicAlgo/MainAlgo.java
@@ -0,0 +1,24 @@
+package BasicAlgo;
+
+public class MainAlgo {
+    public static void main(String[] args) {
+        // Create GA object
+        GeneticAlgorithm ga = new GeneticAlgorithm(100, 0.01, 0.95, 0);
+        Population population = ga.initPopulation(50);
+        // The following is the new code you should be adding:
+        ga.evalPopulation(population);
+        int generation = 1;
+        while (ga.isTerminationConditionMet(population) == false) {
+            // Print fittest individual from population
+            System.out.println("Best solution: " + population.getFittest(0).toString());
+            // TODO! : Apply crossover
+            // TODO! : Apply mutation
+            // DONE! : Evaluate population
+            ga.evalPopulation(population);
+            // Increment the current generation
+            generation++;
+        }
+        System.out.println("Found solution in " + generation + "generations");
+        System.out.println("Best solution: " + population.getFittest(0).toString());
+    }
+}
